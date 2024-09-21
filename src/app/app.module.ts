edf73ffb08f63a2bf50271d3a2e599be8ee5c252
anum shafique <anmshafique704@gmail.com>
2024-09-22 01:09:14 +0500

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,34 @@ import { NgForComponent } from './angular/ng-for/ng-for.component';
 import { StringInterpolationComponent } from './angular/string-interpolation/string-interpolation.component';
 import { EventBindingComponent } from './angular/event-binding/event-binding.component';
 import { TemplateRefrenceComponent } from './angular/template-refrence/template-refrence.component';
+import { HomeComponent } from './angular/home/home.component';
+import { AboutComponent } from './angular/about/about.component';
+import { ContactComponent } from './angular/contact/contact.component';
+import { ProdcutComponent } from './angular/prodcut/prodcut.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './angular/login/login.component';
+import { PageNotFoundComponent } from './angular/page-not-found/page-not-found.component';
+import { LaptopComponent } from './angular/laptop/laptop.component';
+import { TelevisionComponent } from './angular/television/television.component';
+import { MobileComponent } from './angular/mobile/mobile.component';
+import { WatchComponent } from './angular/watch/watch.component';
 
+
+const appRoutes:Routes=[
+  {path: '' , redirectTo:'login', pathMatch:'full'},
+{path: 'home' , component:HomeComponent},
+{path: 'login' , component: LoginComponent},
+{path: 'about' , component:AboutComponent},
+{path: 'contact' , component:ContactComponent},
+{path: 'prodcut'  ,component:ProdcutComponent, children: [
+  {path: 'laptop' , component:LaptopComponent},
+  {path: 'television' , component:TelevisionComponent},
+  {path: 'mobile' , component:MobileComponent},
+  {path: 'watch' , component:WatchComponent},
+]},
+{path: '**' , component:PageNotFoundComponent},
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,13 +47,24 @@ import { TemplateRefrenceComponent } from './angular/template-refrence/template-
     NgForComponent,
     StringInterpolationComponent,
     EventBindingComponent,
-    TemplateRefrenceComponent
+    TemplateRefrenceComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProdcutComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    LaptopComponent,
+    TelevisionComponent,
+    MobileComponent,
+    WatchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
